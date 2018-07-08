3b7a17c85a41dbb6daa5d4f519582aca854dfee5
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-08 22:38:32 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,10 +17,12 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        attribute.delete if {|flavor| flavor == "strawberry"
+        attribute.delete if {|flavor| flavor == "strawberry"}
       end
-
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
 
+puts first_challenge
