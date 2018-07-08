6179523403acd4c44f3a832a219f617fa4271ad3
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-08 22:41:41 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         puts attribute
 
-        attribute.delete_if {|flavor| flavor == "strawberry"}
+        #attribute.delete_if {|flavor| flavor == "strawberry"}
       end
     end
   end
